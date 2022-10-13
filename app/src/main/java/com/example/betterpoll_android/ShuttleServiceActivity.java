2f619e8b105080986c512665ed81a104a024967c
Aditya <adityabisht02@gmail.com>
2022-10-14 01:47:59 +0530

    initialise project

---
+++ app/src/main/java/com/example/betterpoll_android/ShuttleServiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.betterpoll_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShuttleServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shuttle_service);
+    }
+    }
