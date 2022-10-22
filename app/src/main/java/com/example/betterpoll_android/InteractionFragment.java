83dad2b7dc5a848f168223d50988c9566229bd10
Summiedev <sumayah4ever@gmail.com>
2022-10-22 05:47:20 +0100

    Available Shuttle Fragment Modified

--- app/src/main/java/com/example/betterpoll_android/InteractionFragment.java
+++ app/src/main/java/com/example/betterpoll_android/InteractionFragment.java
@@ -2,63 +2,34 @@ package com.example.betterpoll_android;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import com.example.betterpoll_android.databinding.FragmentInteractionSectionBinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InteractionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class InteractionFragment extends Fragment {
+private FragmentInteractionSectionBinding fragmentInteractionSectionBinding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public InteractionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InteractionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InteractionFragment newInstance(String param1, String param2) {
-        InteractionFragment fragment = new InteractionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_interaction_section, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        fragmentInteractionSectionBinding = FragmentInteractionSectionBinding.inflate(inflater, container, false);
+        View root = fragmentInteractionSectionBinding.getRoot();
+
+
+        return root;
+        //return inflater.inflate(R.layout.fragment_video, container, false);
     }
 }
